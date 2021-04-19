85ec7eedd07524a00b3d1b6da80bf124482644a6
toblich <toblich@users.noreply.github.com>
2021-04-19 19:15:04 -0300

    improvements to slackbot

--- src/routers/v1.ts
+++ src/routers/v1.ts
@@ -1,8 +1,23 @@
 import * as express from 'express';
-import * as path from 'path';
-import * as OAS from 'express-openapi-validator';
+import * as service from '../services/service';
+import * as citizenApi from "../services/citizenApi";
 
 const router = express.Router();
+
+const events = {
+  listFlows: 'List flows',
+  describeFlow: 'Describe flows',
+  createFlow: 'Create Flow',
+  deleteFlow: 'Delete Flow',
+  cloneFlow: 'Clone Flow',
+  activateFlow: 'Activate Flow',
+  deactivateFlow: 'Deactivate Flow',
+  scheduleActivationFlow: 'Schedule Flow Activation',
+  scheduleDeactivationFlow: 'Schedule Flow Deactivation',
+  runHistory: 'Get Run History',
+  getLastExecutionErrors: 'Get Last Execution Errors',
+  subscribeToFlowEvents: 'Subscribe To Flow Events',
+}
 
 // const apiSpec = path.join(__dirname, '../../apiSpec.yaml'); // TODO file location
 // router.use('/spec', express.static(apiSpec));
@@ -12,17 +27,45 @@ router.get('/', (req, res) => res.send('Hello world!\n'));
 
 router.post('/', async (req, res) => {
   const body = req.body;
-  console.log(JSON.stringify(body, null, 4));
+  console.log(JSON.stringify(body));
 
   if(body.type === "url_verification") {
     res.json({challenge: body?.challenge || 'hardcoded-value'});
     return;
   }
 
-  console.log('body text', body.text);
+  res.status(200).end(); // Tell slack this was received, process async
 
-  // const response = await service.process(req);
-  // res.send(response);
+  if (body?.event?.bot_id) {
+    console.log('Skipping event as it was posted by a bot', body.event.bot_id);
+    return;
+  }
+
+  if (!(body.event.text as string).includes('<@U01V54MAS49>')) {
+    console.log('Skipping as message is not directed at us');
+    return;
+  }
+
+  const selectedAction = body.event.text;
+
+  let flows;
+  try {
+    flows = await citizenApi.getFlows();
+  } catch (e) {
+    console.error('Error fetching flows', e.stack);
+  }
+
+  const {channel} = body.event;
+  try {
+    service.sendMessage(channel, 'This is a test');
+  } catch(error) {
+    console.log('error', error.stack);
+  }
+});
+
+router.post('/interactive-action', async (req, res) => {
+  console.log(JSON.stringify(req.body, null, 4));
+  res.status(200).end();
 });
 
 export default router;
---
+++ src/services/service.ts
@@ -0,0 +1,130 @@
+import * as fetch from "isomorphic-fetch";
+import * as superagent from "superagent";
+
+const token = process.env.TOKEN;
+const baseUri = process.env.SLACK_API;
+
+if (!token || !baseUri) {
+  console.error('Missing environment variables TOKEN or SLACK_API');
+}
+
+const blocks = [
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "Which flow would you like to see details for?"
+    }
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "My first flow"
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "Select",
+        "emoji": true
+      },
+      "value": "click_me_123",
+      "action_id": "button-action"
+    }
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "My second flow name here"
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "Select",
+        "emoji": true
+      },
+      "value": "flowId:abc-123",
+      "action_id": "button-action"
+    }
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "My second flow name here"
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "Select",
+        "emoji": true
+      },
+      "value": "flowId:abc-123",
+      "action_id": "button-action"
+    }
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "My second flow name here"
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "Select",
+        "emoji": true
+      },
+      "value": "flowId:abc-123",
+      "action_id": "button-action"
+    }
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "My second flow name here"
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "Select",
+        "emoji": true
+      },
+      "value": "flowId:abc-123",
+      "action_id": "button-action"
+    }
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "This is a section block with a button."
+    },
+    "accessory": {
+      "type": "button",
+      "text": {
+        "type": "plain_text",
+        "text": "Select",
+        "emoji": true
+      },
+      "value": "click_me_123",
+      "action_id": "CustomAction"
+    }
+  }
+];
+
+export async function sendMessage(channel: string, text: string) {
+  return superagent.post(`${baseUri}/chat.postMessage`)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Accept', 'application/json')
+    .set('X-Slack-No-Retry', 1)
+    .retry(0)
+    .send({text, channel, blocks});
+}

d19c6bd9a83b2206d22dfdb0ea824333153cb7d2
toblich <toblich@users.noreply.github.com>
2021-04-20 12:03:59 -0300

    Lint fixes to v1.ts

--- src/routers/v1.ts
+++ src/routers/v1.ts
@@ -1,81 +1,81 @@
-import * as express from 'express';
-import * as service from '../services/service';
+import * as express from "express";
+import { FlowSummaryList } from "../common/types/FlowProject";
+import { ActionIds, flowListToBlocks, flowToBlocks } from "../services/blocksTransformer";
 import * as citizenApi from "../services/citizenApi";
-import { ActionIds, flowListToBlocks, flowToBlocks } from '../services/blocksTransformer';
-import { FlowSummaryList } from '../common/types/FlowProject';
-import { inspect } from 'util';
-import { getFlowById } from '../services/citizenApi';
+import { getFlowById } from "../services/citizenApi";
+import * as service from "../services/service";
 
 const router = express.Router();
 
-const events = {
-  listFlows: 'List flows',
-  describeFlow: 'Describe flows',
-  createFlow: 'Create Flow',
-  deleteFlow: 'Delete Flow',
-  cloneFlow: 'Clone Flow',
-  activateFlow: 'Activate Flow',
-  deactivateFlow: 'Deactivate Flow',
-  scheduleActivationFlow: 'Schedule Flow Activation',
-  scheduleDeactivationFlow: 'Schedule Flow Deactivation',
-  runHistory: 'Get Run History',
-  getLastExecutionErrors: 'Get Last Execution Errors',
-  subscribeToFlowEvents: 'Subscribe To Flow Events',
-}
+// const events = {
+//   listFlows: "List flows" ,
+//   describeFlow: 'Describe flows',
+//   createFlow: 'Create Flow',
+//   deleteFlow: 'Delete Flow',
+//   cloneFlow: 'Clone Flow',
+//   activateFlow: 'Activate Flow',
+//   deactivateFlow: 'Deactivate Flow',
+//   scheduleActivationFlow: 'Schedule Flow Activation',
+//   scheduleDeactivationFlow: 'Schedule Flow Deactivation',
+//   runHistory: 'Get Run History',
+//   getLastExecutionErrors: 'Get Last Execution Errors',
+//   subscribeToFlowEvents: 'Subscribe To Flow Events',
+// }
 
-router.get('/', (req, res) => res.send('Hello world!\n'));
+router.get("/", (req, res) => res.send("Hello world!\n"));
 
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const body = req.body;
   console.log(JSON.stringify(body));
 
   if (body.type === "url_verification") {
-    res.json({ challenge: body?.challenge || 'hardcoded-value' });
+    res.json({ challenge: body?.challenge || "hardcoded-value" });
     return;
   }
 
   res.status(200).end(); // Tell slack this was received, process async
 
   if (body?.event?.bot_id) {
-    console.log('Skipping event as it was posted by a bot', body.event.bot_id);
+    console.log("Skipping event as it was posted by a bot", body.event.bot_id);
     return;
   }
 
-  const {text, type} = body.event
+  const { text, type } = body.event;
   if (!text) {
-    console.log('Skipping as it has no text');
+    console.log("Skipping as it has no text");
     return;
   }
 
   const botId = process.env.BOT_ID;
   if (!botId) {
-    throw new Error('Missing BOT_ID');
+    throw new Error("Missing BOT_ID");
   }
-  if (!text.includes(`<@${botId}>`) || type !== 'message') {
-    console.log('Skipping as message is not directed at us or it was not a message');
+  if (!text.includes(`<@${botId}>`) || type !== "message") {
+    console.log("Skipping as message is not directed at us or it was not a message");
     return;
   }
 
-  const selectedAction = (body.event.text as string).toLowerCase().trim().replace(/ +/g, ' ');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const selectedAction = (body.event.text as string).toLowerCase().trim().replace(/ +/g, " ");
 
   let flows: FlowSummaryList | undefined;
   try {
     flows = await citizenApi.getFlows();
   } catch (e) {
-    console.error('Error fetching flows', e.stack);
+    console.error("Error fetching flows", e.stack);
     next(e);
   }
 
   const { channel } = body.event;
   try {
-    service.sendMessage(channel, 'This is a test', flows && flowListToBlocks(flows));
+    service.sendMessage(channel, "This is a test", flows && flowListToBlocks(flows));
   } catch (error) {
-    console.log('error', error.stack);
+    console.log("error", error.stack);
     next(error);
   }
 });
 
-router.post('/interactive-action', async (req, res, next) => {
+router.post("/interactive-action", async (req, res, next) => {
   try {
     setTimeout(() => res.status(200).end(), 1500);
 
@@ -87,9 +87,9 @@ router.post('/interactive-action', async (req, res, next) => {
       const flowId = payload.actions[0].value;
       console.log(`fetching flow id ${flowId}`);
       const flow = await getFlowById(flowId);
-      service.sendMessage(channel, 'This is an action', flowToBlocks(flow));
+      service.sendMessage(channel, "This is an action", flowToBlocks(flow));
     } else {
-      service.sendMessage(channel, 'This is an action', []);
+      service.sendMessage(channel, "This is an action", []);
     }
   } catch (e) {
     next(e);
@@ -98,14 +98,14 @@ router.post('/interactive-action', async (req, res, next) => {
 
 export default router;
 
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const commands = {
-  'list flows': {
+  "list flows": {
     data: citizenApi.getFlows,
-    format: flowListToBlocks
+    format: flowListToBlocks,
   },
-  'describe flows': {
+  "describe flows": {
     data: citizenApi.getFlowById,
-    format: (x) => x
-  }
-}
+    format: (x) => x,
+  },
+};
